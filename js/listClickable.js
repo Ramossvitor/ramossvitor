ad0de2d56b3afd5cc82248a2b6c606946892bbd7
Vitor Silveira Ramos <ramossvitor@gmail.com>
2024-05-04 18:20:20 -0300

    Cria o arquivo com a função que leva para o site destinado a tecnologia clicada

---
+++ js/listClickable.js
@@ -0,0 +1,37 @@
+function useLink(link) {
+    switch(link) {
+        case 'Python':
+            window.open("https://g.co/kgs/3ADeMhH");
+            break;
+        case 'Unity':
+            window.open("https://g.co/kgs/wFCE1hn");
+            break;
+        case 'Javascript':
+            window.open("https://g.co/kgs/FjE94K5");
+            break;
+        case 'PHP':
+            window.open("https://g.co/kgs/MEXWhz1");
+            break;
+        case 'Java':
+            window.open("https://g.co/kgs/9nNkD9K");
+            break;
+        case 'HTML':
+            window.open("https://g.co/kgs/ePd4Tiy");
+            break;
+        case 'CSS':
+            window.open('https://g.co/kgs/niAkcLe');
+            break;
+        case 'Lua':
+            window.open('https://g.co/kgs/ibsFmV6');
+            break;
+        case 'UnrealEngine':
+            window.open('https://g.co/kgs/11BsJHc');
+            break;
+        case 'Blender':
+            window.open("https://g.co/kgs/EoLUHmq");
+            break;
+        case 'WordPress':
+            window.open("https://g.co/kgs/kcj5QtY");
+            break;
+    }
+}
